9b3f01627ce352e8149ce1c2b2da8de1737fbe6a
Monish Shah <tomonishshah@gmail.com>
2026-05-30 12:11:15 +0530

    config tested and started in dev, it works

--- discovery/edge/src/test/java/edge/EdgeApplicationTests.java
+++ discovery/edge/src/test/java/edge/EdgeApplicationTests.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.NONE,
-    properties = {"eureka.client.enabled=false", "spring.cloud.config.enabled=false"}
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {
+        "eureka.client.enabled=false",
+        "spring.cloud.config.enabled=false",
+        "server.ssl.enabled=false"
+    }
 )
 class EdgeApplicationTests {
 
